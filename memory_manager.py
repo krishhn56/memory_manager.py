12e13af279ec86f09bffcead30f007af56704118
krishna <krishhn9123@gmail.com>
2026-04-02 13:56:48 +0530

    Refactor memory management to use file storage

--- memory_manager.py
+++ memory_manager.py
@@ -1,10 +1,45 @@
-memory_store = []
+import file_storage
+import time
 
 def store(user_input, output):
-    memory_store.append({
+    memory = file_storage.load_memory()
+
+    memory.append({
         "input": user_input,
-        "output": output
+        "output": output,
+        "time": time.time()
     })
 
+    file_storage.save_memory(memory)
+
+
+def search_memory(keyword):
+    memory = file_storage.load_memory()
+    results = []
+
+    for item in memory:
+        score = 0
+
+        if keyword.lower() in item["input"].lower():
+            score += 2
+
+        if keyword.lower() in item["output"].lower():
+            score += 1
+
+        if score > 0:
+            item["score"] = score
+            results.append(item)
+
+    results.sort(key=lambda x: x["score"], reverse=True)
+
+    return results
+
+
 def retrieve(user_input):
-    return memory_store[-3:]  # last 3 memory
+    memory = file_storage.load_memory()
+    results = search_memory(user_input)
+
+    if results:
+        return results[:5]   # best 5 (not last)
+
+    return memory[-5:]
